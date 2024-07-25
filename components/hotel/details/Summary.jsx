59968aaf4ccfb5c67a2f620e98488435f65e7f63
sakil-islam <sakilislam1080@gmail.com>
2024-07-26 00:17:43 +0600

    done fully

---
+++ components/hotel/details/Summary.jsx
@@ -0,0 +1,13 @@
+import HotelSummaryInfo from "../HotelSummaryInfo";
+
+const Summary = ({ hotelInfo }) => {
+  return (
+    <section className="py-4 mt-[100px] ">
+      <div className="flex container">
+        <HotelSummaryInfo source="details" info={hotelInfo} />
+      </div>
+    </section>
+  );
+};
+
+export default Summary;
